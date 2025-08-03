ae16fa10e04011bb49c5f2b507f77af4e0973213
mrodavis <orville.davis@scrapit.io>
2025-08-03 15:36:38 -0400

    fixed auto-logId logic

--- utils/flightUtils.js
+++ utils/flightUtils.js
@@ -2,10 +2,12 @@
 
 function generateFlightLogId({ date, airportCode, username }) {
   const formattedDate = new Date(date).toISOString().split('T')[0].replace(/-/g, '');
-  const userPart = username.replace(/\s+/g, '').toUpperCase();
+  const userPart = username.replace(/\s+/g, '').toUpperCase().slice(0,3);
   const code = airportCode.toUpperCase();
 
-  return `FL-${formattedDate}-${code}-${userPart}`;
+  const randomSuffix = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+
+  return `FL-${formattedDate}-${code}-${userPart}-${randomSuffix}`;
 }
 
 module.exports = { generateFlightLogId };
